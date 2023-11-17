6787638c408d0d6fff89d1b00cd64b676d6ff3ca
KarbovskyiV <karbovskyi.volodymyr@gmail.com>
2023-11-17 17:06:00 +0200

    add show route

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Admin\Main\IndexController as AdminIndexController;
 use App\Http\Controllers\Admin\Category\IndexController as CategoryIndexController;
 use App\Http\Controllers\Admin\Category\CreateController as CategoryCreateController;
 use App\Http\Controllers\Admin\Category\StoreController as CategoryStoreController;
+use App\Http\Controllers\Admin\Category\ShowController as CategoryShowController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', IndexController::class);
@@ -16,6 +17,7 @@ Route::prefix('admin')->group(function () {
         Route::get('/', CategoryIndexController::class)->name('admin.category.index');
         Route::get('/create', CategoryCreateController::class)->name('admin.category.create');
         Route::post('/', CategoryStoreController::class)->name('admin.category.store');
+        Route::get('/{category}', CategoryShowController::class)->name('admin.category.show');
     });
 });
 
