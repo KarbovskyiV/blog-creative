23ac4de689c44d302eddbb1b87f805a291283d83
KarbovskyiV <karbovskyi.volodymyr@gmail.com>
2023-11-17 14:04:25 +0200

    add store route

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Main\IndexController;
 use App\Http\Controllers\Admin\Main\IndexController as AdminIndexController;
 use App\Http\Controllers\Admin\Category\IndexController as CategoryIndexController;
 use App\Http\Controllers\Admin\Category\CreateController as CategoryCreateController;
+use App\Http\Controllers\Admin\Category\StoreController as CategoryStoreController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', IndexController::class);
@@ -14,6 +15,7 @@ Route::prefix('admin')->group(function () {
     Route::prefix('categories')->group(function () {
         Route::get('/', CategoryIndexController::class)->name('admin.category.index');
         Route::get('/create', CategoryCreateController::class)->name('admin.category.create');
+        Route::post('/', CategoryStoreController::class)->name('admin.category.store');
     });
 });
 
