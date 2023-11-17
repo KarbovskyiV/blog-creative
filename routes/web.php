53edfcc36e97a9e20b5296a96bc73e514061a87e
KarbovskyiV <karbovskyi.volodymyr@gmail.com>
2023-11-17 18:43:15 +0200

    add delete route

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\Admin\Category\StoreController as CategoryStoreControll
 use App\Http\Controllers\Admin\Category\ShowController as CategoryShowController;
 use App\Http\Controllers\Admin\Category\EditController as CategoryEditController;
 use App\Http\Controllers\Admin\Category\UpdateController as CategoryUpdateController;
+use App\Http\Controllers\Admin\Category\DeleteController as CategoryDeleteController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', IndexController::class);
@@ -22,6 +23,7 @@ Route::prefix('admin')->group(function () {
         Route::get('/{category}', CategoryShowController::class)->name('admin.category.show');
         Route::get('/{category}/edit', CategoryEditController::class)->name('admin.category.edit');
         Route::patch('/{category}', CategoryUpdateController::class)->name('admin.category.update');
+        Route::delete('/{category}', CategoryDeleteController::class)->name('admin.category.delete');
     });
 });
 
