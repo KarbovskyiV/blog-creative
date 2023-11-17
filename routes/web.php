8642899fd08b119cd260c7fb26d557d045ab2182
KarbovskyiV <karbovskyi.volodymyr@gmail.com>
2023-11-17 17:26:52 +0200

    add edit route

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Admin\Category\IndexController as CategoryIndexControll
 use App\Http\Controllers\Admin\Category\CreateController as CategoryCreateController;
 use App\Http\Controllers\Admin\Category\StoreController as CategoryStoreController;
 use App\Http\Controllers\Admin\Category\ShowController as CategoryShowController;
+use App\Http\Controllers\Admin\Category\EditController as CategoryEditController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', IndexController::class);
@@ -18,6 +19,7 @@ Route::prefix('admin')->group(function () {
         Route::get('/create', CategoryCreateController::class)->name('admin.category.create');
         Route::post('/', CategoryStoreController::class)->name('admin.category.store');
         Route::get('/{category}', CategoryShowController::class)->name('admin.category.show');
+        Route::get('/{category}/edit', CategoryEditController::class)->name('admin.category.edit');
     });
 });
 
