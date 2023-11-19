2f4b0c2919cd6bd9fc4024a99a71be1e0ca51c3f
KarbovskyiV <karbovskyi.volodymyr@gmail.com>
2023-11-19 12:39:42 +0200

    add crud route for tags

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,13 @@ use App\Http\Controllers\Admin\Category\ShowController as CategoryShowController
 use App\Http\Controllers\Admin\Category\EditController as CategoryEditController;
 use App\Http\Controllers\Admin\Category\UpdateController as CategoryUpdateController;
 use App\Http\Controllers\Admin\Category\DeleteController as CategoryDeleteController;
+use App\Http\Controllers\Admin\Tag\IndexController as TagIndexController;
+use App\Http\Controllers\Admin\Tag\CreateController as TagCreateController;
+use App\Http\Controllers\Admin\Tag\StoreController as TagStoreController;
+use App\Http\Controllers\Admin\Tag\ShowController as TagShowController;
+use App\Http\Controllers\Admin\Tag\EditController as TagEditController;
+use App\Http\Controllers\Admin\Tag\UpdateController as TagUpdateController;
+use App\Http\Controllers\Admin\Tag\DeleteController as TagDeleteController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', IndexController::class);
@@ -16,7 +23,7 @@ Route::get('/', IndexController::class);
 Route::prefix('admin')->group(function () {
     Route::get('/', AdminIndexController::class);
 
-    Route::prefix('categories')->group(function () {
+    Route::prefix('category')->group(function () {
         Route::get('/', CategoryIndexController::class)->name('admin.category.index');
         Route::get('/create', CategoryCreateController::class)->name('admin.category.create');
         Route::post('/', CategoryStoreController::class)->name('admin.category.store');
@@ -24,6 +31,16 @@ Route::prefix('admin')->group(function () {
         Route::get('/{category}/edit', CategoryEditController::class)->name('admin.category.edit');
         Route::patch('/{category}', CategoryUpdateController::class)->name('admin.category.update');
         Route::delete('/{category}', CategoryDeleteController::class)->name('admin.category.delete');
+    });
+
+    Route::prefix('tags')->group(function () {
+        Route::get('/', TagIndexController::class)->name('admin.tag.index');
+        Route::get('/create', TagCreateController::class)->name('admin.tag.create');
+        Route::post('/', TagStoreController::class)->name('admin.tag.store');
+        Route::get('/{tag}', TagShowController::class)->name('admin.tag.show');
+        Route::get('/{tag}/edit', TagEditController::class)->name('admin.tag.edit');
+        Route::patch('/{tag}', TagUpdateController::class)->name('admin.tag.update');
+        Route::delete('/{tag}', TagDeleteController::class)->name('admin.tag.delete');
     });
 });
 
