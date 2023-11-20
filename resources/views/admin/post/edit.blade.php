8243bafdd4843c2085b77dc1cbf7fde16dee6610
KarbovskyiV <karbovskyi.volodymyr@gmail.com>
2023-11-20 11:35:56 +0200

    add edit table with all fields

--- resources/views/admin/post/edit.blade.php
+++ resources/views/admin/post/edit.blade.php
@@ -25,19 +25,95 @@
                 <!-- Small boxes (Stat box) -->
                 <div class="row">
                     <div class="col-12">
-                        <form action="{{ route('admin.post.update', $post->id) }}" method="post" class="w-25">
+                        <form action="{{ route('admin.post.update', $post->id) }}" method="post" enctype="multipart/form-data">
                             @csrf
                             @method('patch')
-                            <div class="form-group">
+                            <div class="form-group w-25">
                                 <input type="text" class="form-control" name="title" placeholder="Name of post"
-                                       value="{{ $post->title }}">
+                                       value="{{ $post->title }}"
+                                >
+                                @error('title')
+                                <div class="text-danger">
+                                    {{ $message }}
+                                </div>
+                                @enderror
                             </div>
-                            @error('title')
-                            <div class="text-danger">
-                                {{ $message }}
+                            <div class="form-group">
+                                <textarea id="summernote" name="content">
+                                    {{ $post->content }}
+                                </textarea>
+                                @error('content')
+                                <div class="text-danger">
+                                    {{ $message }}
+                                </div>
+                                @enderror
                             </div>
-                            @enderror
-                            <input type="submit" class="btn btn-primary" value="Save">
+                            <div class="form-group w-50">
+                                <label>Add preview</label>
+                                <div class="w-25 mb-2">
+                                    <img src="{{ asset('storage/' . $post->preview_image) }}" alt="preview_image" class="w-50">
+                                </div>
+                                <div class="input-group">
+                                    <div class="custom-file">
+                                        <input type="file" class="custom-file-input" name="preview_image">
+                                        <label class="custom-file-label">Choose file</label>
+                                    </div>
+                                    <div class="input-group-append">
+                                        <span class="input-group-text">Upload</span>
+                                    </div>
+                                </div>
+
+                                @error('preview_image')
+                                <div class="text-danger">
+                                    {{ $message }}
+                                </div>
+                                @enderror
+                            </div>
+                            <div class="form-group w-50">
+                                <label>Add main image</label>
+                                <div class="w-50 mb-2">
+                                    <img src="{{ url('storage/' . $post->main_image) }}" alt="main_image" class="w-50">
+                                </div>
+                                <div class="input-group">
+                                    <div class="custom-file">
+                                        <input type="file" class="custom-file-input" name="main_image">
+                                        <label class="custom-file-label">Choose file</label>
+                                    </div>
+                                    <div class="input-group-append">
+                                        <span class="input-group-text">Upload</span>
+                                    </div>
+                                </div>
+
+                                @error('main_image')
+                                <div class="text-danger">
+                                    {{ $message }}
+                                </div>
+                                @enderror
+                            </div>
+                            <div class="form-group w-50">
+                                <label>Choose category</label>
+                                <select name="category_id" class="form-control">
+                                    @foreach($categories as $category)
+                                        <option value="{{ $category->id }}"
+                                            {{ $category->id == $post->category_id ? ' selected' : '' }}
+                                        >{{ $category->title }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                            <div class="form-group">
+                                <label>Tags</label>
+                                <select class="select2" name="tag_ids[]" multiple="multiple"
+                                        data-placeholder="Select tags"
+                                        style="width: 100%;">
+                                    @foreach($tags as $tag)
+                                        <option
+                                            {{ is_array($post->tags->pluck('id')->toArray()) && in_array($tag->id, $post->tags->pluck('id')->toArray()) ? ' selected' : '' }} value="{{ $tag->id }}">{{ $tag->title }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                            <div class="form-group">
+                                <input type="submit" class="btn btn-primary" value="Save">
+                            </div>
                         </form>
                     </div>
                 </div>
