93824e231a8d2506382273542abdedcf21b16410
KarbovskyiV <karbovskyi.volodymyr@gmail.com>
2023-11-20 13:42:31 +0200

    create base post controller

---
+++ app/Http/Controllers/Admin/Post/BaseController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Post;
+
+use App\Http\Controllers\Controller;
+use App\Services\PostService;
+
+class BaseController extends Controller
+{
+    public function __construct(public PostService $service)
+    {
+    }
+}
