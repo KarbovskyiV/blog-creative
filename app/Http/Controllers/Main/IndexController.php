a81db61737e645f48b81aea7ee9ffa278f4eb5ea
KarbovskyiV <karbovskyi.volodymyr@gmail.com>
2023-11-23 18:41:22 +0200

    freeze main index

--- app/Http/Controllers/Main/IndexController.php
+++ app/Http/Controllers/Main/IndexController.php
@@ -3,16 +3,11 @@
 namespace App\Http\Controllers\Main;
 
 use App\Http\Controllers\Controller;
-use App\Models\Post;
 
 class IndexController extends Controller
 {
     public function __invoke()
     {
-        $posts = Post::query()->paginate(6);
-        $randomPosts = Post::query()->get()->random(4);
-        $likedPosts = Post::query()->withCount('likedUsers')->orderByDesc('liked_users_count')->get()->take(4);
-
-        return view('main.index', compact('posts', 'randomPosts', 'likedPosts'));
+        return redirect()->route('post.index');
     }
 }
