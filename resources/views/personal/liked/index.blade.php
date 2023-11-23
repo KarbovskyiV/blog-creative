50a2bed7bf2b0ddd15c55667a1f70e825ceed7a6
KarbovskyiV <karbovskyi.volodymyr@gmail.com>
2023-11-23 11:47:48 +0200

    add basic posts

--- resources/views/personal/liked/index.blade.php
+++ resources/views/personal/liked/index.blade.php
@@ -23,7 +23,46 @@
             <div class="container-fluid">
                 <!-- Small boxes (Stat box) -->
                 <div class="row">
-
+                    <div class="col-6">
+                        <div class="card">
+                            <!-- /.card-header -->
+                            <div class="card-body table-responsive p-0">
+                                <table class="table table-hover text-nowrap">
+                                    <thead>
+                                    <tr>
+                                        <th>ID</th>
+                                        <th>Title</th>
+                                        <th colspan="2" class="text-center">Action</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    @foreach($posts as $post)
+                                        <tr>
+                                            <td>{{ $post->id }}</td>
+                                            <td>{{ $post->title }}</td>
+                                            <td class="text-center">
+                                                <a href="{{ route('admin.post.show', $post->id) }}">
+                                                    <i class="far fa-eye"></i>
+                                                </a>
+                                            </td>
+                                            <td>
+                                                <form action="{{ route('personal.liked.delete', $post->id) }}"
+                                                      method="post">
+                                                    @csrf
+                                                    @method('delete')
+                                                    <button type="submit" class="border-0 bg-transparent">
+                                                        <i class="fas fa-trash text-danger" role="button"></i>
+                                                    </button>
+                                                </form>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
+                            <!-- /.card-body -->
+                        </div>
+                    </div>
                 </div>
             </div><!-- /.container-fluid -->
         </section>
