15dd4c48489c532f78d7a1a09f2a014046b086f3
KarbovskyiV <karbovskyi.volodymyr@gmail.com>
2023-11-24 11:14:42 +0200

    add relationship with users, add carbon getter

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -2,8 +2,10 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Comment extends Model
 {
@@ -11,4 +13,14 @@ class Comment extends Model
 
     protected $table = 'comments';
     protected $guarded = false;
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function getDateAsCarbonAttribute(): Carbon
+    {
+        return Carbon::parse($this->created_at);
+    }
 }
